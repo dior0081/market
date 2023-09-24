8888ace758cff570b1c8c94afb1496feb7634c9a
dior0081 <103140454+dior0081@users.noreply.github.com>
2023-09-24 15:35:53 +0700

    Market
    Market

---
+++ voyage.tj/js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	function fullHeight(){
+		var winHeight = $(window).height();
+		$('.start_s').height(winHeight);
+	};
+	fullHeight();
+	$(window).resize(function(){
+		fullHeight();
+	});
+	
+});
+$(document).ready(function) {
+
+	$("#slider")
+}
+
